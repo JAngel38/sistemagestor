3249cfc74e12930fc411427037f1884d0e4ff709
JAngel38 <angabconbai@gmail.com>
2025-06-03 21:39:49 -0600

    Update db_connector.py
    Separar mas las variables

--- db_connector.py
+++ db_connector.py
@@ -3,12 +3,16 @@ import mysql.connector
 def conectar_bd(usuario, clave, base_datos):
     try:
         conn = mysql.connector.connect(
+            
             host="192.168.1.65",
             user="admon",
             password="123456",
             database="gestpro1"
+            
         )
+        
         return conn
+        
     except mysql.connector.Error as err:
         print("Error:", err)
         return None
